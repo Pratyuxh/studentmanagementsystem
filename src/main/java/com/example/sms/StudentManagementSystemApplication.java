0611ae421b3ef54781d0bc32dae2e551f5be51f2
Pratyuxxh <107082402+Pratyuxxh@users.noreply.github.com>
2022-09-29 21:06:41 +0530

    Initial commit

---
+++ src/main/java/com/example/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello This is my first project");
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+}
